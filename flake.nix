c307c9fb1777cee2fd3995961a5cfa35aa97b09c
dvill <villalbautria08@gmail.com>
2025-08-15 21:33:21 -0500

    chore: goreleaser setup

--- flake.nix
+++ flake.nix
@@ -3,16 +3,22 @@
 
   inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
   inputs.flake-utils.url = "github:numtide/flake-utils";
+  inputs.nur = {
+      url = "github:nix-community/NUR";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
 
   outputs = { self, ... }@inputs:
     inputs.flake-utils.lib.eachDefaultSystem (system:
       let
         pkgs = inputs.nixpkgs.legacyPackages.${system};
+        nurpkgs = inputs.nur.legacyPackages.${system};
       in
       {
         devShells.default = pkgs.mkShell {
           packages = [
             pkgs.go
+            nurpkgs.repos.goreleaser.goreleaser
           ];
         };
       }
